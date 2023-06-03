7334712f1aff133df531975af1dd25896c5fa2bc
swarajjaguar <swarajswaraj128@gmail.com>
2023-06-03 12:06:49 +0530

    message

---
+++ src/main/java/com/persondetails/Pancard.java
@@ -0,0 +1,40 @@
+package com.persondetails;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Pancard {
+
+	@Id
+	private int id;
+	private String name;
+	private String add;
+	private String panno;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAdd() {
+		return add;
+	}
+	public void setAdd(String add) {
+		this.add = add;
+	}
+	public String getPanno() {
+		return panno;
+	}
+	public void setPanno(String panno) {
+		this.panno = panno;
+	}
+	
+	
+}
